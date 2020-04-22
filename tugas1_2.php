f323958093949af20f7fecf9e247232b3ba9a292
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2020-04-22 10:38:45 +0800

    Tugas 1_2 PHP
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas1_2.php
@@ -0,0 +1,43 @@
+<?php
+$harga1 = "Rp.1000";
+$harga2 = "Rp.15000";
+$harga3 = "Rp.20000";
+$harga4 = "Rp.5000";
+$harga5 = "Rp.1000";
+
+echo "<h1>Daftar Menu</h1>";
+echo "<table border=1>
+  <tr>
+    <th>No</th>
+    <th>Nama Menu</th>
+    <th>Harga</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Cireng</td>
+    <td>$harga1</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Es Duren</td>
+    <td>$harga2</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Es Milo</td>
+    <td>$harga3</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Es Teh</td>
+    <td>$harga4</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>Piscok</td>
+    <td>$harga5</td>
+  </tr>
+</table>
+";
+
+ ?>

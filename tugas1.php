131fd786a7c96f82a878fbc618958f731832b3f1
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2020-04-22 10:33:48 +0800

    Tugas 1 PHP
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas1.php
@@ -0,0 +1,23 @@
+<?php
+$harga1 = 13000;
+$harga2 = 15000;
+$harga3 = 13000;
+$harga4 = 1000;
+$harga5 = 1000;
+$harga6 = 3000;
+$harga7 = 5000;
+$harga8 = 4000;
+$harga9 = 5000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "<li>Ayam Goreng &#160;&#160;= Rp.$harga1</li>";
+echo "<li>Ayam Bakar &#160;&#160;&#160;&#160;= Rp. $harga2</li>";
+echo "<li>Ayam Sayur &#160;&#160;&#160;&#160;= Rp. $harga3</li>";
+echo "<li>Tempe Goreng = Rp. $harga4</li>";
+echo "<li>Tahu Goreng  &#160;&#160;&#160;= Rp. $harga5</li>";
+echo "<li>Soto &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;= Rp. $harga6</li>";
+echo "<li>Nasi Putih &#160;&#160;&#160;&#160;&#160;&#160;&#160;= Rp. $harga7</li>";
+echo "<li>Es Teh &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;= Rp. $harga8</li>";
+echo "<li>Es Tebuh &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;= Rp. $harga9</li>";
+
+ ?>
